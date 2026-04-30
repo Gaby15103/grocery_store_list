62e9881094f8964de7215d38485bfa39034a2a4b
gaby15103 <gabmorin@gmail.com>
2026-04-30 18:29:25 -0400

    feat: fix delete item to delete list.

--- backend/routes/listRoutes.js
+++ backend/routes/listRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const itemCtrl = require('../controllers/itemController');
 
 router.post('/', itemCtrl.createList);
-router.delete('/:listId', itemCtrl.deleteItem);
+router.delete('/:listId', itemCtrl.deleteList);
 router.get('/:listId/items', itemCtrl.getListItems);
 
 module.exports = router;
